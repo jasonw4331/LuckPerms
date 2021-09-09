37bc1f79e05963581dfb724d7c7e243393db43aa
jasonwynn10 <jasonwynn10@me.com>
2021-09-08 20:45:29 -0400

    Added custom build script for registry automation

--- build/generate-registry-annotations.php
+++ build/generate-registry-annotations.php
@@ -21,45 +21,70 @@
 
 declare(strict_types=1);
 
-namespace pocketmine\build\update_registry_annotations;
-
 use pocketmine\utils\RegistryUtils;
-use function basename;
-use function class_exists;
-use function count;
-use function dirname;
-use function file_get_contents;
-use function file_put_contents;
-use function preg_match;
-use function str_replace;
-use function substr;
 
 if(count($argv) !== 2){
 	die("Provide a path to process");
 }
 
+require dirname(__DIR__) . '/../../vendor/autoload.php'; // should be valid assuming folder plugin
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($argv[1], \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::CURRENT_AS_PATHNAME)) as $file){
 	if(substr($file, -4) !== ".php"){
+		echo 'Exit -1'.PHP_EOL;
 		continue;
 	}
+	echo $file.PHP_EOL;
 	$contents = file_get_contents($file);
 	if($contents === false){
 		throw new \RuntimeException("Failed to get contents of $file");
 	}
 
 	if(preg_match("/^namespace (.+);$/m", $contents, $matches) !== 1 || preg_match('/^((final|abstract)\s+)?class /m', $contents) !== 1){
+		echo 'Exit 0'.PHP_EOL;
 		continue;
 	}
 	$shortClassName = basename($file, ".php");
 	$className = $matches[1] . "\\" . $shortClassName;
+	echo $className.PHP_EOL;
 	if(!class_exists($className)){
-		continue;
+		echo 'Exit ~1'.PHP_EOL;
+		if(strpos($className, 'FlagUtil') !== false)
+			continue;
+		if(strpos($className, 'ConfigKeys') !== false)
+			continue;
+		if(strpos($className, 'AbstractContextSet') !== false)
+			continue;
+		if(strpos($className, 'ImmutableContextSetImpl') !== false)
+			continue;
+		if(strpos($className, 'QueryOptionsBuilderImpl') !== false)
+			continue;
+		if(strpos($className, 'QueryOptionsImpl') !== false)
+			continue;
+		if(strpos($className, 'SimpleMetaValueSelector') !== false)
+			continue;
+		if(strpos($className, 'DepthFirstIterator') !== false)
+			continue;
+		if(strpos($className, 'MetaStackDefinition') !== false)
+			continue;
+		if(strpos($className, 'AllParentsByWeight') !== false)
+			continue;
+		if(strpos($className, 'ParentsByWeight') !== false)
+			continue;
+		if(strpos($className, 'ParentsByWeight') !== false)
+			continue;
+		require $file;
+
+		if(!class_exists($className)){
+			echo 'Exit 1'.PHP_EOL;
+			continue;
+		}
 	}
 	$reflect = new \ReflectionClass($className);
 	$docComment = $reflect->getDocComment();
 	if($docComment === false || (preg_match("/^\s*\*\s*\@see .+::_generateMethodAnnotations\(\)$/m", $docComment) !== 1 && preg_match("/^\s*\*\s*@generate-registry-docblock$/m", $docComment) !== 1)){
+		echo 'Exit 2'.PHP_EOL;
 		continue;
 	}
 	echo "Found registry in $file\n";
