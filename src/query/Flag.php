fade505e77c54e483498baf44f95710342934526
jasonwynn10 <jasonwynn10@me.com>
2022-12-28 00:51:17 -0500

    Fixed registry annotations

--- src/query/Flag.php
+++ src/query/Flag.php
@@ -7,7 +7,8 @@ use pocketmine\utils\EnumTrait;
 /**
  * This doc-block is generated automatically, do not modify it manually.
  * This must be regenerated whenever registry members are added, removed or changed.
- * @see \pocketmine\utils\RegistryUtils::_generateMethodAnnotations()
+ * @see build/generate-registry-annotations.php
+ * @generate-registry-docblock
  *
  * @method static Flag APPLY_INHERITANCE_NODES_WITHOUT_SERVER_CONTEXT()
  * @method static Flag APPLY_INHERITANCE_NODES_WITHOUT_WORLD_CONTEXT()
