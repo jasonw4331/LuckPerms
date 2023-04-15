8002669f4639325eee5140cd7beea7e7cf295cae
jasonwynn10 <jasonwynn10@me.com>
2023-04-15 12:39:41 -0400

    rename generic template in Bound

--- src/config/generic/key/Bound.php
+++ src/config/generic/key/Bound.php
@@ -8,18 +8,18 @@ namespace jasonwynn10\LuckPerms\config\generic\key;
 use jasonwynn10\LuckPerms\config\generic\adapter\ConfigurationAdapter;
 
 /**
- * @template T
+ * @template Tvalue
  */
 class Bound{
 
 	/**
-	 * @param ConfigKeyFactory<T> $factory
-	 * @param T                   $def
+	 * @param ConfigKeyFactory<Tvalue> $factory
+	 * @param Tvalue                   $def
 	 */
 	public function __construct(private ConfigKeyFactory $factory, private string $path, private mixed $def){ }
 
 	/**
-	 * @return T
+	 * @return Tvalue
 	 */
 	public function __invoke(ConfigurationAdapter $adapter) : mixed{
 		return $adapter::{$this->factory->getFunctionName()}($this->path, $this->def);
